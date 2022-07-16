6b68818244ee2ad7ebf0825a59d29c3cf8348065
Michael Helvey <michael@michaelhelvey.dev>
2022-07-16 09:04:50 -0500

    chore(deploy): set up dev only prisma override

--- start.sh
+++ start.sh
@@ -6,5 +6,6 @@
 # Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
 
 set -ex
+npx prisma migrate reset --force # development only so that I can break things before deploying
 npx prisma migrate deploy
 npm run start
